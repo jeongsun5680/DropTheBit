42ac0677c0120dfd99a82f83b002c7df63120f73
JissuPark <qkrwltn9412@gmail.com>
2021-03-30 09:22:56 +0900

    test apis

--- binance_api/binance.py
+++ binance_api/binance.py
@@ -1,4 +1,7 @@
+import json
+
 import requests
+from bs4 import BeautifulSoup
 
 base_url = "https://api.binance.com"
 
@@ -24,7 +27,24 @@ def priceInfo():
     return res.json()
 
 
+def upbit():
+    res = requests.get("https://upbit.com/vi/candle/minutes/1?", params={})
+    return res.text
+
+
+def binance():
+    binc_url = 'https://www.binance.com/ko/trade/BTC_USDT?layout=basic'
+    res = requests.get(binc_url)
+    soup = BeautifulSoup(res.text, 'html.parser')  # 바이낸스 파서하기
+    content = soup.find("script", {"id": "__APP_DATA"}).string
+    return json.dumps(json.loads(content), indent=4)
+
+
+
 if __name__ == "__main__":
+    '''
     print(connectivity())
     print(exchangInfo())
     print(priceInfo())
+    '''print(upbit())
+    #print(binance())
