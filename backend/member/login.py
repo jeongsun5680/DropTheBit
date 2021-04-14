9c8fcd1fe674d58e066b71fd4187f657cdd1fcae
jeongsun <wjdjeong4067261@gmail.com>
2021-04-14 17:45:34 +0900

    'url_view_connect'

--- backend/member/login.py
+++ backend/member/login.py
@@ -3,23 +3,22 @@ import json
 
 # from MyApplication.MongoDbManager import MongoDbManager
 
-def get_login(request):
-    user_id = request.POST('user_id') # 이거 view에서 넘겨받아야 한다
-    user_pw = request.POST('user_pw')
+def get_login(user_id, user_pw):
+    
     # 만약 파라미터만 다르게 해도 되면 get_user_info
     # dbUserData = MongoDbManager().get_user_info_all({'user_id':user_id, 'user_pw':user_pw}) -> # 없는 정보면 dbUserData의 리턴값이 null 일지, 아니면 특정한 default의 뭐가 올지 모르겠다.. 우선 if문으로 처리하자
     if dbUserData is None:
         # dbUserData = MongoDbManager().get_user_info_id({'user_id':user_id})
         #if dbUserData is None:
-        #    # return HttpResponse("회원정보가 존재하지 않습니다", status = 404)
+        #    # return 404, "회원정보가 존재하지 않습니다"
         #    pass
         #else:
-        #    # return HttpResponse("비밀번호가 틀렸습니다", status = 400)
+        #    # return 400, "비밀번호가 틀렸습니다"
         #    pass
         pass
     else:
         # del dbUserData['_id'] -> 임의로 오는 '_id'라는 것을 지워주고 리턴
-        # return HttpResponse(json.dumps(dbUserData), status=200) #그냥 status 값만 보내주면 된다, 나중에 json 삭제 필요
+        # return 200, json.dumps(dbUserData) #그냥 status 값만 보내주면 된다, 나중에 json 삭제 필요
         pass
 
 
